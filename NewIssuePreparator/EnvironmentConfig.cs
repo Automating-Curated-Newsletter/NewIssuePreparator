190e906fbb1e83a29389872bf6ba3d4a4a59d374
Mariusz <klimcio@gmail.com>
2025-05-17 21:30:59 +0200

    store secret names in private consts

--- NewIssuePreparator/EnvironmentConfig.cs
+++ NewIssuePreparator/EnvironmentConfig.cs
@@ -15,6 +15,12 @@ namespace NewIssuePreparator
 
         public class AirtableConfig
         {
+            private const string API_ACCESS_TOKEN_KEY = "AIRTABLE_API_ACCESS_TOKEN";
+            private const string BASE_ID_KEY = "AIRTABLE_BASE_ID";
+            private const string TABLE_NAME_KEY = "AIRTABLE_TABLE_NAME";
+            private const string API_URL_KEY = "AIRTABLE_API_URL";
+            private const string VIEW_ID_KEY = "AIRTABLE_VIEW_ID";
+
             public string ApiAccessToken { get; }
             public string BaseId { get; }
             public string TableName { get; }
@@ -23,34 +29,37 @@ namespace NewIssuePreparator
             
             public AirtableConfig()
             {
-                ApiAccessToken = Environment.GetEnvironmentVariable("AIRTABLE_API_ACCESS_TOKEN") ?? 
-                    throw new ArgumentNullException("AIRTABLE_API_ACCESS_TOKEN is not set in environment variables");
-                BaseId = Environment.GetEnvironmentVariable("AIRTABLE_BASE_ID") ?? 
-                    throw new ArgumentNullException("AIRTABLE_BASE_ID is not set in environment variables");
-                TableName = Environment.GetEnvironmentVariable("AIRTABLE_TABLE_NAME") ?? 
-                    throw new ArgumentNullException("AIRTABLE_TABLE_NAME is not set in environment variables");
-                ApiUrl = Environment.GetEnvironmentVariable("AIRTABLE_API_URL") ?? 
-                    throw new ArgumentNullException("AIRTABLE_API_URL is not set in environment variables");
-                FetchFromView = Environment.GetEnvironmentVariable("AIRTABLE_VIEW_ID") ?? 
-                    throw new ArgumentNullException("AIRTABLE_VIEW_ID is not set in environment variables");
-                
+                ApiAccessToken = Environment.GetEnvironmentVariable(API_ACCESS_TOKEN_KEY) ?? 
+                    throw new ArgumentNullException($"{API_ACCESS_TOKEN_KEY} is not set in environment variables");
+                BaseId = Environment.GetEnvironmentVariable(BASE_ID_KEY) ?? 
+                    throw new ArgumentNullException($"{BASE_ID_KEY} is not set in environment variables");
+                TableName = Environment.GetEnvironmentVariable(TABLE_NAME_KEY) ?? 
+                    throw new ArgumentNullException($"{TABLE_NAME_KEY} is not set in environment variables");
+                ApiUrl = Environment.GetEnvironmentVariable(API_URL_KEY) ?? 
+                    throw new ArgumentNullException($"{API_URL_KEY} is not set in environment variables");
+                FetchFromView = Environment.GetEnvironmentVariable(VIEW_ID_KEY) ?? 
+                    throw new ArgumentNullException($"{VIEW_ID_KEY} is not set in environment variables");
             }
         }
 
         public class CuratedConfig
         {
+            private const string API_TOKEN_KEY = "CURATED_API_TOKEN";
+            private const string API_URL_KEY = "CURATED_API_URL";
+            private const string PUBLICATION_ID_KEY = "CURATED_PUBLICATION_ID";
+
             public string ApiToken { get; }
             public string ApiUrl { get; }
             public string PublicationId { get; }
 
             public CuratedConfig()
             {
-                ApiToken = Environment.GetEnvironmentVariable("CURATED_API_TOKEN") ?? 
-                    throw new ArgumentNullException("CURATED_API_TOKEN is not set in environment variables");
-                ApiUrl = Environment.GetEnvironmentVariable("CURATED_API_URL") ?? 
-                    throw new ArgumentNullException("CURATED_API_URL is not set in environment variables");
-                PublicationId = Environment.GetEnvironmentVariable("CURATED_PUBLICATION_ID") ?? 
-                    throw new ArgumentNullException("CURATED_PUBLICATION_ID is not set in environment variables");
+                ApiToken = Environment.GetEnvironmentVariable(API_TOKEN_KEY) ?? 
+                    throw new ArgumentNullException($"{API_TOKEN_KEY} is not set in environment variables");
+                ApiUrl = Environment.GetEnvironmentVariable(API_URL_KEY) ?? 
+                    throw new ArgumentNullException($"{API_URL_KEY} is not set in environment variables");
+                PublicationId = Environment.GetEnvironmentVariable(PUBLICATION_ID_KEY) ?? 
+                    throw new ArgumentNullException($"{PUBLICATION_ID_KEY} is not set in environment variables");
             }
         }
     }
