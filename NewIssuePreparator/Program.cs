e3a9c280a0344fbd8445902066990468a36961ba
Mariusz <klimcio@gmail.com>
2025-06-21 14:44:01 +0200

    Changed migration status + changed debug messages

--- NewIssuePreparator/Program.cs
+++ NewIssuePreparator/Program.cs
@@ -1,9 +1,9 @@
-using System;
-
 namespace NewIssuePreparator;
 
 class Program
 {
+    private const string ReadyForMigrationStatus = "Ready for Curated";
+
     static async Task Main(string[] args)
     {
         try
@@ -12,16 +12,16 @@ class Program
 
             var records = await config.Airtable.GetContentAsync();
 
-            Console.WriteLine($"Records downloaded: {records?.Records.Count()}");
+            Console.WriteLine($"Records downloaded from AirTable: {records?.Records.Count()}");
 
             var nextIssueRecords = records?.Records
-                .Where(x => AirTableRecordExtensions.Status(x) == "Next issue")
+                .Where(x => AirTableRecordExtensions.Status(x) == ReadyForMigrationStatus)
                 .Where(x => !string.IsNullOrWhiteSpace(x.TargetCategory()))
                 .Take(config.Processing.BatchSize)
                 .Select(CuratedLink.Create)
                 .ToList();
 
-            Console.WriteLine($"Records found: {nextIssueRecords?.Count}");
+            Console.WriteLine($"Records ready to be migrated: {nextIssueRecords?.Count}");
 
             foreach(var link in nextIssueRecords!)
             {
