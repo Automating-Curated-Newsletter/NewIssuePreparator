322022233f899bca9baeff8df160b308dd4941fb
Mariusz <klimcio@gmail.com>
2025-06-21 14:55:28 +0200

    Remove unused code and comments in Program.cs
    Cleaned up Program.cs by deleting commented-out code and unused constants to improve readability and maintainability.

--- NewIssuePreparator/Program.cs
+++ NewIssuePreparator/Program.cs
@@ -2,8 +2,6 @@ namespace NewIssuePreparator;
 
 class Program
 {
-    // private const string ReadyForMigrationStatus = "Ready for Curated";
-
     static async Task Main(string[] args)
     {
         try
@@ -15,7 +13,6 @@ class Program
             Console.WriteLine($"Records downloaded from AirTable: {records?.Records.Count()}");
 
             var nextIssueRecords = records?.Records
-                // .Where(x => AirTableRecordExtensions.Status(x) == ReadyForMigrationStatus)
                 .Where(x => !string.IsNullOrWhiteSpace(x.TargetCategory()))
                 .Take(config.Processing.BatchSize)
                 .Select(CuratedLink.Create)
